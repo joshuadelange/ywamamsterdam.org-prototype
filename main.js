aebcc465acc5f8d364ee2b11deb4bb47a8bdd6ba
Joshua de Lange <joshua.delange@gmail.com>
2013-10-09 11:12:16 +0200

    increases video loading

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
 
+  $('video').bind('playing', function(){
+    $(this).show() ;
+    $(this).parent().find('img').remove() ;
+  }) ;
+
   $('.slideshow').click(function(ev){
 
     var $this = $(this),
