ddcc02184da8eef76979c63070834499ee587642
Joshua de Lange <joshua.delange@gmail.com>
2013-10-10 15:14:51 +0200

    finalizes navigation, footer

--- main.js
+++ main.js
@@ -1,40 +1,32 @@
 $(document).ready(function(){
 
-  $('footer .y').click(function(){
-    console.log('doubleclick!') ;
-    $('body').append('<img src="rboss.jpg" class="rboss">') ;
-    $('.rboss').animate({left: '78.5%'}) ;
-    setTimeout(function(){
-      $('.rboss').animate({left: '100%'}, function(){
-        $('.rboss').remove() ;
-      }) ;
-    }, 2000)
-  }) ;
-
   $(window).scroll(function(ev){
+
     var headerHeight = 720,
         scrollTop = $(document).scrollTop(),
         opacity = (700 / scrollTop) / 10,
+        marginTop = 20 - scrollTop,
         reverseOpacity = 0.95 - opacity ;
 
-    console.log('opacity', opacity) ;
-    console.log('reverseOpacity', reverseOpacity) ;
+    console.log('marginTop', marginTop) ;
+
+    if(marginTop > 0){
+      $('.top').css('marginTop', 20 - scrollTop + 'px') ;
+    }
+    else {
+      $('.top').css('marginTop', 0 + 'px') ;
+    }
 
     if(opacity > 0) {
 
-      // $('video, header h1').css('opacity', opacity) ;
       $('.top').css('backgroundColor', 'rgba(247, 247, 247, ' + reverseOpacity + ')') ;
       $('.top').css('borderBottomColor', 'rgba(226, 226, 226, ' + reverseOpacity + ')') ;
 
       if(opacity > 0.45) {
-        $('nav ul li').css({'font-family': 'Novecentowide-Light'}) ;
-        $('.top nav ul li a').css({'color': 'white', 'font-size': '17px'}) ;
-        $('.top nav ul li a:hover').css({'color': 'white', 'border-bottom-color': 'white'}) ;
+        $('.top nav ul li a').removeClass('with-white-bg') ;
       }
       else {
-        $('nav ul li').css({'font-family': 'Novecentowide-Normal'}) ;
-        $('.top nav ul li a').css({'color': '#474747', 'font-size': '15px'}) ;
-        $('.top nav ul li a:hover').css({'color': '#474747', 'border-bottom-color': '#474747'}) ;
+        $('.top nav ul li a').addClass('with-white-bg') ;
       }
   
     }
