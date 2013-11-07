e9597c00e3cce5e0f3f0be2dce0f9e52cb2279ad
Joshua de Lange <joshua.delange@gmail.com>
2013-11-07 20:36:20 +0100

    adds story.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
+var $ = $ ;
+
 $(document).ready(function(){
 
   $(window).scroll(function(ev){
 
+    /*
     var headerHeight = 720,
         scrollTop = $(document).scrollTop(),
         opacity = (700 / scrollTop) / 10,
@@ -32,6 +35,7 @@ $(document).ready(function(){
       }
   
     }
+    */
 
   }) ;
 
@@ -53,6 +57,65 @@ $(document).ready(function(){
     else {
       //go forward
       $this.find('.image-wrapper').animate({marginLeft: '+=' + imageWidth}) ;
+    }
+
+  }) ;
+
+  $('.timeline a.marker').click(function(){
+
+    var $this = $(this),
+        $timeline = $this.closest('.timeline'),
+        id = $this.data('jump-to'),
+        slideIndex = $timeline.find('#' + id).index(),
+        slideWidth = parseInt($timeline.find('.slide:first-of-type').css('width'), 10),
+        cursorLeft = parseInt($this.css('left'), 10),
+        nextMargin = (slideIndex * slideWidth) * -1 ;
+
+        $timeline.find('.cursor').animate({left: cursorLeft}) ;
+        $timeline.find('.slides').animate({marginLeft: nextMargin}) ;
+
+  }) ;
+
+  $('.timeline .slide').click(function(ev){
+    
+    var $this = $(this) ;
+
+    if($this.get(0).tagName !== 'A') {
+
+      var center = window.innerWidth / 2,
+          $slide = $(this).closest('.slide'),
+          slideWidth = parseInt($slide.css('width'), 10),
+          $slides = $this.closest('.slides'),
+          numOfSlides = $slides.find('.slide').length,
+          currentMargin = parseInt($slides.css('marginLeft'), 10),
+          nextMargin = currentMargin,
+          maxMargin = (numOfSlides * slideWidth) * -1,
+
+          //cursor stuff
+          id = $slide.attr('id'),
+          $timeline = $(this).closest('.timeline'),
+          $marker = $timeline.find('[data-jump-to="' + id + '"]'),
+          $nextMarker = $marker ;
+
+      if(ev.pageX > center) {
+        //go forward
+        $nextMarker = $marker.next() ;
+        nextMargin = currentMargin - slideWidth ;
+      }
+      else {
+        //go backward
+        $nextMarker = $marker.prev() ;
+        nextMargin = currentMargin + slideWidth ;
+      }
+
+      var cursorLeft = $nextMarker.css('left') ;
+
+      $timeline.find('.cursor').animate({left: cursorLeft}) ;
+
+      if(nextMargin <= 0 && nextMargin > maxMargin) {
+        $this.closest('.slides').animate({marginLeft: nextMargin}) ;
+      }
+
     }
 
   }) ;
